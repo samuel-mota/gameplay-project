a83c2edaf42cfeb278e5dd8639469ad3f53abb16
Samuel Mota <smagnun@gmail.com>
2021-07-04 12:38:22 +0200

    profile and avatar components

--- App.tsx
+++ App.tsx
@@ -10,6 +10,7 @@ import AppLoading from "expo-app-loading";
 
 import { SignIn } from "./src/screens/SignIn";
 import { Background } from "./src/components/Background";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -27,7 +28,8 @@ export default function App() {
     <Background>
       <StatusBar />
       {/* <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />  imported from react native*/}
-      <SignIn />
+      {/* <SignIn /> */}
+      <Home />
     </Background>
   );
 }
