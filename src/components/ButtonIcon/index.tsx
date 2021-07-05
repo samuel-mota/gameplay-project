4914f6af445dffc9c06b782718a446a53adde41b
Samuel Mota <smagnun@gmail.com>
2021-07-05 20:01:59 +0200

    change TouchableOpacity to ReactButton on ButtonIcon and added handleSignIn on Signin screen

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,26 +1,21 @@
 import React from "react";
-import {
-  Text,
-  Image,
-  View,
-  TouchableOpacity,
-  TouchableOpacityProps,
-} from "react-native";
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
+import { Text, Image, View } from "react-native";
 
 import DiscordIcon from "../../assets/discord.png";
 import { styles } from "./styles";
 
-type ButtonProps = TouchableOpacityProps & {
+type ButtonProps = RectButtonProps & {
   title: string;
 };
 
 export function ButtonIcon({ title, ...rest }: ButtonProps) {
   return (
-    <TouchableOpacity style={styles.container} {...rest}>
+    <RectButton style={styles.container} {...rest}>
       <View style={styles.iconWrapper}>
         <Image source={DiscordIcon} style={styles.icon} />
       </View>
       <Text style={styles.title}>Entrar com Discord</Text>
-    </TouchableOpacity>
+    </RectButton>
   );
 }
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigation } from "@react-navigation/native";
 import { View, Text, Image } from "react-native";
 
 import IllustrationImg from "../../assets/illustration.png";
@@ -6,6 +7,12 @@ import { styles } from "./styles";
 import { ButtonIcon } from "../../components/ButtonIcon";
 
 export function SignIn() {
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate("Home");
+  }
+
   return (
     <View style={styles.container}>
       <Image
@@ -25,7 +32,7 @@ export function SignIn() {
           favoritos com seus amigos
         </Text>
 
-        <ButtonIcon title="Entrar com Discord" activeOpacity={0.7} />
+        <ButtonIcon title="Entrar com Discord" onPress={handleSignIn} />
       </View>
     </View>
   );
