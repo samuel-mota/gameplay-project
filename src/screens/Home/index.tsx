4eefa0afca9227cefdafaf67e4cfa596f886c00f
Samuel Mota <smagnun@gmail.com>
2021-07-09 19:48:35 +0200

    added category selected component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { View } from "react-native";
-import { ButtonAdd } from "../../components/ButtonAdd";
 
+import { CategorySelect } from "../../components/CategorySelect";
+import { ButtonAdd } from "../../components/ButtonAdd";
 import { Profile } from "../../components/Profile";
 
 import { styles } from "./styles";
 
 export function Home() {
+  const [category, setCategory] = useState("");
+
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory("") : setCategory(categoryId); // para marcar e desmarcar
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
         <Profile />
         <ButtonAdd />
+      </View>
+
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
+
+        <View style={styles.content}></View>
       </View>
     </View>
   );
