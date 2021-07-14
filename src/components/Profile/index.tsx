b7f194ed4a040440700c519181ab8fd496eb8a0c
Samuel Mota <smagnun@gmail.com>
2021-07-14 20:51:34 +0200

    auth DISCORD - user data is already config on Profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,19 +1,22 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { useAuth } from "../../hooks/auth";
 
 import { Avatar } from "../Avatar";
 import { styles } from "./styles";
 
 export function Profile() {
+  const { user } = useAuth();
+
   return (
     <View style={styles.container}>
-      <Avatar urlImage="https://github.com/samuel-mota.png" />
+      <Avatar urlImage={user.avatar} />
 
       <View>
         <View style={styles.user}>
           <Text style={styles.greetings}>Olá,</Text>
 
-          <Text style={styles.username}>Samuel</Text>
+          <Text style={styles.username}>{user.firstName}</Text>
         </View>
 
         <Text style={styles.message}>Hoje é dia de vitória!</Text>
